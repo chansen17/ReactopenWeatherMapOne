6225f0fdbac35d1b6ed76c55a4cc562577a5ce25
chansen17 <44001658+chansen17@users.noreply.github.com>
2018-12-19 03:21:27 -0800

    Initial commit
    Production

---
+++ build/precache-manifest.25d2a07df134f20e1569a0e743bff4d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fcee20485486e4d94660",
+    "url": "/static/css/main.d3cf5c24.chunk.css"
+  },
+  {
+    "revision": "fcee20485486e4d94660",
+    "url": "/static/js/main.fcee2048.chunk.js"
+  },
+  {
+    "revision": "769e228087a92a571f8d",
+    "url": "/static/js/1.769e2280.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3af1e56e3ebdde3149e67644ac8ecc32",
+    "url": "/index.html"
+  }
+];
